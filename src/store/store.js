521f622c6c9a8574bd1f068871817fc92afad631
uiwook <dmldnr1230@yu.ac.kr>
2025-09-12 14:29:20 +0900

    250912 Redux 기본 구조

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import festivalReducer from './slices/festivalSlice.js'
+
+export default configureStore({
+  reducer: {
+    festival: festivalReducer,
+    
+  }
+})
