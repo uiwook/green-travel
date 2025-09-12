88653b2a28415c3ec37c132564d34e671d91a4af
uiwook <dmldnr1230@yu.ac.kr>
2025-09-12 11:14:37 +0900

    250912 [구현] FESTIVAL_MAIN01 작업 완료

---
+++ src/conponents/Main.jsx
@@ -0,0 +1,16 @@
+import './Main.css'
+import titleImg from '../assets/Sungnyemun.jpg'
+// import { useNavigate } from 'react-router-dom';
+
+function Main() {
+  // const navigate = useNavigate();
+
+  return (
+    <>
+      <img className='title-img' src={titleImg} alt="대문" />
+      {/* onClick={() => {navigate('/festivals')}} */}
+    </>
+  )
+}
+
+export default Main;
